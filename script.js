e5f458b86581d86c2565d65e97f022b37b9ca8c3
niccbytes <140906373+niccbytes@users.noreply.github.com>
2023-09-05 18:26:08 -0500

    added some things still not functioning

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // i defined the function and i console.logged inorder to make sure the button works
 function generatePassword(){
   console.log('you clicked me') 
+  
 
 // all my code goes in here to meet the acceptance criteria and then then when you return it it handles the display on the screen
 // 1.) promot the user
@@ -10,16 +11,30 @@ function generatePassword(){
 let passwordLength = prompt(`Choose a password length between 8 and 128 characters.`); // this is the variable prompt to ask for the lenght of the password
 
 if (passwordLength >= 8 && passwordLength <= 128) { // this if statment then defines how short and long it needs to be
+  let includeUpperCase = confirm(`would you like to include uppercase letters?`);
+  let includeLowerCase = confirm(`would you like to include lowercase letters?`);
+  let includeNumbers = confirm(`would you like to include numbers in this password?`);
+  let includeSpecialCharacters = confirm(`would you like to include any special characters in this password?`)
 
-}
-// a.) password lenght 8<128
-// b.) uppercase lowercase special char and numbers
+
+// charachter set 
+let characters = '';
+    if (includeUpperCase) characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (includeLowerCase) characters += 'abcdefghijklmnopqrstuvwxyz';
+    if (includeNumbers) characters += '0123456789';
+    if (includeSpecialCharacters) characters += '!@#$%^&*()_+{}[]|:;"<>,.?/~';
+
+
+// a.) password lenght 8<128 DONE
+// b.) uppercase lowercase special char and numbers prompt DONE
+
 // 2.) validate the input
+
+
 // 3.) display the password on the page
 
-  return 'heres the password hoe' // this shows my password in the box
-}
-
+  return (``); // this shows my password in the box
+} 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
